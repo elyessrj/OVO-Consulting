a8d36e2ee3f9206ab25199e7cfe8a75f0c471efc
Elyess Rjafellah <elyessou@MacBook-Air-de-Elyess.local>
2023-03-28 16:36:35 +0200

    add

--- models/DbConnect.class.php
+++ models/DbConnect.class.php
@@ -11,13 +11,8 @@ class DbConnect extends PDO{
 
     public function __construct()
     {
-<<<<<<< HEAD
-        $dsn="mysql:host=localhost;dbname=ovo;port=3306;charset=utf8";
-        $username="root";
-=======
         $dsn="mysql:host=localhost;dbname=ovo_consulting;port=8889;charset=utf8";
         $username="elyess";
->>>>>>> 458cee6746fd65fd77f75a4c95507143176bb6ab
         $pass="";
         $options=[];
         parent::__construct($dsn,$username,$pass,$options);
